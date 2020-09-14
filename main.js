acbf1b3ead9380b4fca35da545fff55407def393
skylerkk <kuretza.skyler@gmail.com>
2020-09-14 16:16:30 -0400

    Finished digital clock

---
+++ main.js
@@ -0,0 +1,25 @@
+setInterval(updateTime, 1000);
+function updateTime(){
+    let timer = new Date();
+    let hour = timer.getHours();
+    let minute = timer.getMinutes();
+    let second = timer.getSeconds();
+    var format;
+
+    if (hour > 12){
+        hour -= 12;
+        format = "PM";
+    }
+    else if (hour == 0){
+        hour = 12;
+        format = "AM";
+    }
+    
+    minute = minute < 10 ? "0" + minute : minute;
+    second = second < 10 ? "0" + second: second;
+
+    let currentTime = hour + ":" + minute + ":" + second + " " + format;
+
+    document.getElementById("clock").innerHTML = currentTime;
+}
+updateTime();
